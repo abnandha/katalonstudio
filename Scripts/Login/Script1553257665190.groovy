d81874380cadec6cb3fdc7575a8b500582bb6163
anandha <anandha@AI-PC-136.IRISBSL.COM>
2019-03-22 19:42:21 +0530

    Add smoke test

--- Scripts/Login/Script1553257665190.groovy
+++ Scripts/Login/Script1553257665190.groovy
@@ -14,17 +14,30 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+'Open Browser'
 WebUI.openBrowser('')
 
+'Hit URL to Browser'
 WebUI.navigateToUrl('https://dev.esp.irisgst.com/')
 
+'Maximize the browser tab'
+WebUI.maximizeWindow()
+
+'Enter input to user name'
 WebUI.setText(findTestObject('Login/input_SIGN UP_name'), 'amit.nandha@irisindia.net')
 
+'Enter input to Password'
 WebUI.setEncryptedText(findTestObject('Login/input_Invalid Email_email'), 'RfES2L809yyqtWvN0P2gPg==')
 
+'Click on sumbit button'
 WebUI.click(findTestObject('Login/button_SIGN IN'))
 
-WebUI.click(findTestObject('Login/button_OK'))
+'Get toaster message after getting login'
+LoginTitle = WebUI.getText(findTestObject('Login/div_Login Successful'))
 
+'Verify the login success message and cofirm login '
+WebUI.verifyMatch('LoginTitle', 'Login Successful!', false)
+
+'Close the browser '
 WebUI.closeBrowser()
 
---
+++ Test Suites/Somke Test.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Somke Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>f766d40a-18c6-452a-8b61-0c0720e7bc9e</testSuiteGuid>
+   <testCaseLink>
+      <guid>56ddab3c-8214-4515-b026-efbdb0da8082</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
